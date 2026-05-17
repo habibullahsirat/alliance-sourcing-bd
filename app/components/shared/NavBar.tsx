0c6630b4890b08957c52ab3a8ed9c892e0684428
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-17 13:30:15 +0600

    Update Nav bar.

--- app/components/shared/NavBar.tsx
+++ app/components/shared/NavBar.tsx
@@ -1,3 +1,22 @@
+import { NavLink } from "react-router";
+
 export default function NavBar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between m-4 p-4">
+      <div className="flex justify-start gap-2">
+        <h2>Logo 1</h2>
+        <h2>Logo 2</h2>
+      </div>
+      <div className="flex justify-between gap-8">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">About US</NavLink>
+        <NavLink to="/">Buying House</NavLink>
+        <NavLink to="/">Factory & Machinery</NavLink>
+        <NavLink to="/">Global Partners</NavLink>
+      </div>
+      <div>
+        <button>Speak with us</button>
+      </div>
+    </div>
+  );
 }
