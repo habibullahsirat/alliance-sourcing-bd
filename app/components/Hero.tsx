32281dc76223ab5b448c5ed7699878e7b0a5c7a9
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-18 18:04:48 +0600

    Design updated.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,23 +2,28 @@ export default function Hero() {
   return (
     <div>
       <div className="relative ">
-        <div className="w-full h-82 overflow-hidden shrink-0">
+        <div className="w-full h-160 overflow-hidden shrink-0">
           <img
             src="../../public/hero.png"
             alt="Cloth"
-            className="w-full object-cover"
+            className="w-full h-full object-cover"
           />
         </div>
         <div className="flex flex-col justify-center absolute inset-0 text-center">
           <div className="mx-[40%] w-[20%]">
-            <p className="font-inter text-white bg-white/10 backdrop-blur-md rounded-[30px] p-3 mb-6">
+            <p className="font-[inter] text-white bg-white/10 backdrop-blur-md rounded-[30px] p-3 mb-6">
               House of fashion stitching
             </p>
           </div>
-          <div className="mx-[30%]">
-            <h2 className="text-5xl text-white font-syne">
+          <div className="mx-[25%]">
+            <h2 className="text-[84px] text-white font-[syne]">
               Your trusted partner in apparel sourcing
             </h2>
+            <p className="text-white font-[inter] text-sm mt-5">
+              We connect international buyers with reliable manufacturers across
+              Bangladesh. From product development to final shipment, we handle
+              every detail with precision and care.
+            </p>
           </div>
         </div>
       </div>
