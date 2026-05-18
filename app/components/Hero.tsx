900ae4812b9d954e9f8f3eadb0d644c9b01176fb
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-18 17:20:06 +0600

    Design Updated.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,20 @@
 export default function Hero() {
   return (
     <div>
-      <div className="relative overflow-hidden">
-        <div>
-          <img src="../../public/hero.png" alt="Cloth" className="w-full" />
+      <div className="relative ">
+        <div className="w-full h-82 overflow-hidden shrink-0">
+          <img
+            src="../../public/hero.png"
+            alt="Cloth"
+            className="w-full object-cover"
+          />
         </div>
         <div className="flex flex-col justify-center absolute inset-0 text-center">
-          <p className="font-inter text-white">House of fashion stitching</p>
+          <div className="mx-[40%] w-[20%]">
+            <p className="font-inter text-white bg-white/10 backdrop-blur-md rounded-[30px] p-3 mb-6">
+              House of fashion stitching
+            </p>
+          </div>
           <div className="mx-[30%]">
             <h2 className="text-5xl text-white font-syne">
               Your trusted partner in apparel sourcing
