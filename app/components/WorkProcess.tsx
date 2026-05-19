138025cf38197d5c14a4b0fed03979de64a34e21
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-19 15:32:12 +0600

    Design Updated.

--- app/components/WorkProcess.tsx
+++ app/components/WorkProcess.tsx
@@ -1,3 +1,23 @@
 export default function WorkProcess() {
-  return <div>Work Process</div>;
+  return (
+    <div>
+      <div className="m-[16px] px-[64px] py-[112px] flex justify-between">
+        <div>
+          <p className="text-[16px] font-semibold">Process</p>
+          <h1 className="text-[60px]">How we work</h1>
+          <button className="text-[18px] border-1 border-gray-200 px-[8px] py-[16px] rounded-[5px] w-[116px] leading-[16px mt-[36px]">
+            Discuss
+          </button>
+        </div>
+        <div>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>We listen to your needs and understands your specifications</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
