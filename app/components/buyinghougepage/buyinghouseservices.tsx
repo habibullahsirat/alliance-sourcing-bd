4df187b70b3d0d6346083ecd7d3c498d8213a6bc
Habibullah Sirat <habibullah.sirat.001@gmail.com>
2026-05-20 18:17:13 +0600

    Code refined.

--- app/components/buyinghougepage/buyinghouseservices.tsx
+++ app/components/buyinghougepage/buyinghouseservices.tsx
@@ -19,19 +19,17 @@ const services = [
 export default function BuyingHouseService() {
   return (
     <div className="mx-[32px] px-[16px] py-[32px] mt-[64px]">
-      {/* Header */}
       <div className="text-center mb-10">
         <p>Services</p>
         <h1 className="text-[60px] font-[400]">Buying house services</h1>
         <p>We manage every step of your sourcing journey with precision.</p>
       </div>
 
-      {/* Cards */}
       <div className="grid grid-cols-4 gap-[32px]">
         <div className="bg-[#0C97D5] text-white rounded-md w-[296px] h-[328px] px-[8px] py-[16px]">
           <div className="flex flex-col justify-between px-[8px] py-[16px] h-[217px]">
             <div className="w-[40px]">
-              <img src="../../public/vector12.png" alt="" />
+              <img src="/vector12.png" alt="" />
             </div>
             <div className="mt-[35%]">
               <h2 className="text-[26px]">Product Development and sampling</h2>
@@ -47,12 +45,10 @@ export default function BuyingHouseService() {
             className="rounded-md border border-gray-200 w-[296px] h-[328px] px-[8px] py-[16px]"
           >
             <div className="flex flex-col justify-between px-[8px] py-[16px] h-[217px]">
-              {/* Icon */}
               <div className="w-[40px]">
                 <img src={item.image} alt={item.title} />
               </div>
 
-              {/* Text */}
               <div className="mt-[35%]">
                 <h2 className="text-[26px]">{item.title}</h2>
                 <p className="text-[18px]">{item.description}</p>
